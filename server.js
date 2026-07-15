4f35a4c763e3b95f7d89baee5004330b85eb31b7
Garg-Harsh7558 <harshagrawal7558@gmail.com>
2026-07-15 12:11:54 +0530

    vercel.json

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-
-import "dotenv/config"
+import "dotenv/config";
 import app from "./src/app.js";
 import ConnectDB from "./src/DB_Connection/ConnectDB.js";
+
 const PORT = process.env.PORT || 5000;
 await ConnectDB()
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
